a17a0a23d33bd28381f47903e2cda6d04821cd18
TimRas <martinusras95@gmail.com>
2023-04-07 17:26:43 +0000

    Heroku deployment test

--- fit_for_all/settings.py
+++ fit_for_all/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fit-for-all.herokuapp.com']
+ALLOWED_HOSTS = ['fit-for-all.herokuapp.com', 'localhost']
 
 
 # Application definition
