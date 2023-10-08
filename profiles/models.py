7acedae81908b897c826700ebc748dcfb63b20af
TimRas <martinusras95@gmail.com>
2023-10-08 18:51:49 +0000

    Add profile form, model and update view and template

--- profiles/models.py
+++ profiles/models.py
@@ -1,25 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
+    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
     default_delivery_address = models.CharField(max_length=255)
-    order_history = models.ManyToManyField('Order', blank=True)
 
     def __str__(self):
         return self.user.username
-
-
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    order_date = models.DateTimeField(auto_now_add=True)
-    # Add other fields related to the order, such as products, quantities, etc.
-
-    def __str__(self):
-        return f"Order #{self.id} by {self.user.username}"
 
 
 @receiver(post_save, sender=User)
@@ -33,8 +32,9 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     else:
         # If an existing user is saved, update their user profile.
-        instance.userprofile.save()
+        profile, _ = UserProfile.objects.get_or_create(user=instance)
 
 
-# Connect the signal to the User model
+# # Connect the signal to the User model
 post_save.connect(create_or_update_user_profile, sender=User)
+
