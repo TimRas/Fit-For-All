e63852682b2045eb9faf1326f0e5e2225d7a12b4
TimRas <martinusras95@gmail.com>
2023-10-13 15:30:03 +0000

    Add admin add product feature

--- products/views.py
+++ products/views.py
@@ -6,6 +6,7 @@ from django.db.models.functions import Lower
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Product, Category
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -75,6 +76,7 @@ def auth_check_buy_product(request, product_id):
         return redirect('account_signup')
 
 
+@login_required
 def product_detail(request, product_id):
     """
     Grabs product with product_id and
@@ -88,3 +90,29 @@ def product_detail(request, product_id):
         }
 
     return render(request, 'products/product_detail.html', context)
+
+
+@login_required
+def add_product(request):
+    """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
