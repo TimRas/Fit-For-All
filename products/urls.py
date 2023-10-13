8189b7bafc26fef868435accc35437a3165059fe
TimRas <martinusras95@gmail.com>
2023-10-13 15:49:51 +0000

    Add admin edit product feature

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('buy/<int:product_id>/', views.auth_check_buy_product,
          name='buy_product'),
-    path('<int:product_id>/details', views.product_detail, name='product_detail'),
+    path('details/<int:product_id>/', views.product_detail,
+         name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
