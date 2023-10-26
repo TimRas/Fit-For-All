023f42d87da770334ab57ff8980385ccf339e69c
TimRas <martinusras95@gmail.com>
2023-10-26 19:05:19 +0000

    Adjust css and html of 404 page and set debug to false.

--- fit_for_all/settings.py
+++ fit_for_all/settings.py
@@ -16,7 +16,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['fit-for-all.herokuapp.com', 'localhost', '8000-timras-fitforall-uorvs6zqfl4.ws-eu105.gitpod.io']
 
